199126ea005a7c594cd906d925766ee4bc84f972
Dan Sergeev <yungcatx@gmail.com>
2020-04-09 18:14:50 +0300

    Rmv local truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -2,11 +2,5 @@ const path = require("path");
 
 module.exports = {
   contracts_build_directory: path.join(__dirname, "/node_modules/pointxio-contracts/build/contracts"),
-  networks: {
-    development: {
-     host: "127.0.0.1",     // Localhost (default: none)
-     port: 7545,            // Standard Ethereum port (default: none)
-     network_id: "*",       // Any network (default: none)
-    }
-  }
+  networks: {}
 };
